a15c80bfc1750ddb0ba0cc84fd134837fd25787f
kdharanmani31 <kdharanmani31@gmail.com>
2026-09-08 19:16:25 +0530

    Time: 1 ms (83.65%), Space: 42.8 MB (8.32%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countCommas(int n) {
+        int totalCommas = 0;
+        long base = 1000;
+        
+        while (n >= base) {
+            totalCommas += (n - base + 1);
+            base *= 1000;
+        }
+        
+        return totalCommas;
+    }
+}
